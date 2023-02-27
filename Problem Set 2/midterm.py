2cb61df630f512540dc1c3c2784ff663b68e0fb9
Anchana-Mathew <125073562+Anchana-Mathew@users.noreply.github.com>
2023-02-27 00:03:16 -0500

    Add files via upload

---
+++ Problem Set 2/midterm.py
@@ -0,0 +1,19 @@
+def file_length(file_name):
+    file = open(file_name)
+    contents = file.read()
+    file.close()
+    print(len(contents))
+    file_length('midterm.py')
+
+def file_length(file_name):
+    file = open(file_name)
+    contents = file.read()
+    file.close()
+    print(len(contents))
+    
+
+def file_length(file_name):
+    file = open(file_name)    
+    
+    
+    
